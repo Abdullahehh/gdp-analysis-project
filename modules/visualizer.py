e7bb52c83d1aaef2e13909bd869d990fdc67ec1b
Abdullahehh <abdullahjavaidaries2022@gmail.com>
2026-02-05 22:55:01 +0500

    Added function to create line charts

--- modules/visualizer.py
+++ modules/visualizer.py
@@ -22,4 +22,27 @@ def pie_chart(data,title,path):
     plt.close()
     print(f" Pie chart saved: {path}")
 
+def line_chart(data,title,x,y,path):
+    """Make a line chart"""
+    if not data:
+        print("No data to create line chart!")
+        return
+    
+    plt.figure(figsize=(14,6))
+
+    items=sorted(data.items())
+
+    a=[i[0] for i in items]
+    b=[i[1] for i in items]
+
+    plt.plot(a,b,marker='o',linewidth=2,markersize=8,color='green')
+
+    plt.title(title,fontsize=16,fontweight='bold')
+    plt.xlabel(x,fontsize=12)
+    plt.ylabel(y,fontsize=12)
+    plt.grid(True,alpha=0.3)
+
+    plt.savefig(path,dpi=300,bbox_inches='tight')
+    plt.close()
+    print(f" Line chart saved: {path}")
 
