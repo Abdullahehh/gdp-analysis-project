18ee30480a00c7f45fda3b5f5818720dc061339e
Minahil Mirza <minahilmirza948@email.com>
2026-02-08 19:21:19 +0500

    making csv file out of dictoniaries

--- modules/data_processor.py
+++ modules/data_processor.py
@@ -1,46 +1,13 @@
+import csv
 
-def clean_data(df):
-    """
-    Clean GDP dataset:
-    - Remove rows with missing GDP values
-    - Ensure GDP columns are numeric
-    """
-    df = df.copy()
+def save_long_data(long_data, output_file):
+    if not long_data:
+        return
 
-    # Convert all year columns to numeric where possible
-    year_cols = [col for col in df.columns if isinstance(col, int)]
-
-    df[year_cols] = df[year_cols].apply(
-        lambda x: pd.to_numeric(x, errors="coerce")
-    )
-
-    # Drop rows where all GDP values are missing
-    df = df.dropna(subset=year_cols, how="all")
-
-    return df
-
-def processGDP(df, config):
-
-    continent =config["region"]
-    filterregion=df[df["Continent"]==continent]
-    print(filterregion)
-    year=config["year"]
-    yeardata=filterregion[year]
-    print(yeardata)
-
-    operation=config["operation"]
-
-    if operation=="average":
-        result=yeardata.mean()
-
-    elif operation=="sum":
-        result=yeardata.sum()
-
-    else:
-        result = none
-        print("invalid operation")
-
-    return result
-
-    def reshape_data(df):
-    pass
+    with open(output_file, 'w', newline='', encoding='utf-8') as f:
+        writer = csv.DictWriter(
+            f,
+            fieldnames=long_data[0].keys()
+        )
+        writer.writeheader()
+        writer.writerows(long_data)
