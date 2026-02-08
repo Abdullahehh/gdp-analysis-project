9f3ab634ca0a8d3ee60527a9d734fff1599ba6c9
Minahil Mirza <minahilmirza948@email.com>
2026-02-08 19:43:58 +0500

    filtering load data on basis of config file data

--- modules/data_processor.py
+++ modules/data_processor.py
@@ -11,3 +11,12 @@ def save_long_data(long_data, output_file):
         )
         writer.writeheader()
         writer.writerows(long_data)
+
+
+def filter_data(long_data,config)
+
+    return list(filter(
+        lambda x:(x["continent"]==config["region"])
+                and (x["year"]==int(config["year"])
+                and (config["country"] is none or x[country]==config[country]),long_data
+    ))
