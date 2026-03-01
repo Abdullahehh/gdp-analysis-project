1150bcda028dbeaf7d0c84afe55aa2634d2355ce
Minahil Mirza <minahilmirza948@email.com>
2026-03-01 09:07:42 +0500

    add pipeline class for executing

--- core/contracts.py
+++ core/contracts.py
@@ -6,3 +6,10 @@ class DataSink(Protocol):
    # jo bhi class write(results: dict) method rakhegi, use DataSink maana jayega.
     def write(self, results: dict) -> None:
         ...
+
+        
+@runtime_checkable
+class PipelineService(Protocol):
+  #koi bhi class jo execute(raw_data: List[Any]) method rakhe, use PipelineService maana jayega.
+    def execute(self, raw_data: List[Any]) -> None:
+        ...
